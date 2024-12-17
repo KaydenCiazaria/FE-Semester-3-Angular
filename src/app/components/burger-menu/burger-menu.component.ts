85fced61d67a4d62a60ea6703ae10c35978ddccb
ghjknehbor <troydevdan@gmail.com>
2024-12-18 00:23:36 +0700

    working burger menu

--- src/app/components/burger-menu/burger-menu.component.ts
+++ src/app/components/burger-menu/burger-menu.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { PopAreYouSureComponent } from '../popups/pop-are-you-sure/pop-are-you-sure.component';
 
 @Component({
   selector: 'app-burger-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PopAreYouSureComponent],
   templateUrl: './burger-menu.component.html',
   styleUrl: './burger-menu.component.css'
 })
+
 export class BurgerMenuComponent {
+
+  isOpen: boolean = false;
+  logoutModal: boolean = false;
+
+  constructor(private router: Router){}
+
+  handleMenuClick() {
+    this.isOpen = !this.isOpen;
+  }
+
+  handleMenuClose() {
+    this.isOpen = false;
+  }
+
+  @Output() onLogoutClick = new EventEmitter<boolean>();
+
+  toggleLogoutModal() {
+    this.logoutModal = !this.logoutModal;
+    console.log(`logoutModal is now: ${this.logoutModal}`)
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([route])
+  }
 
 }
