9cb19565468d686c3e4a3e9beaa37b5900d6192d
Bryant <bryanteffendi1234@gmail.com>
2024-12-17 20:55:55 +0700

    Made auth button,property info and list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { PopLoginComponent } from './components/popups/pop-login/pop-login.component';
+import { AuthButtonsComponent } from './components/auth-buttons/auth-buttons.component';
+import { PropertyInformationComponent } from './components/property-information/property-information.component';
 import { FormRegisterComponent } from './components/form/form-register/form-register.component';
 import {FormAddPropertyComponent} from './components/form/form-add-property/form-add-property.component';
 import { FormReservationComponent } from './components/form/form-reservation/form-reservation.component';
@@ -11,4 +13,6 @@ export const routes: Routes = [
   { path: 'form-add-property', component: FormAddPropertyComponent },
   { path: 'reservation', component: FormReservationComponent },  // this is the final: { path: 'reservation/:id', component: FormReservationComponent },
   { path: 'edit-villa', component: FormEditPropertyComponent }, // add /:id
+  { path: 'auth-button', component: AuthButtonsComponent },
+  { path: 'property-info', component: PropertyInformationComponent },
 ];
